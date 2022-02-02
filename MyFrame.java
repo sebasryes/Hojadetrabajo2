5855e23d677e24b1c0a44b3a6d2d1b9b7147df09
robertriosm <87707371+robertriosm@users.noreply.github.com>
2022-02-02 17:18:39 -0600

    ya muestra el resultado
    ya solo faltaría la implementación de calculate

--- MyFrame.java
+++ MyFrame.java
@@ -17,8 +17,9 @@ import java.io.IOException;
 public class MyFrame extends JFrame implements ActionListener {
 
     private JPanel bg;
-    private JLabel label, tittle, resultado;
+    private JLabel label, tittle, result;
     private JButton chooseButton;
+    private Pila pila;
 
     public MyFrame() {
         this.setSize(400, 300);
@@ -52,10 +53,10 @@ public class MyFrame extends JFrame implements ActionListener {
         tittle.setAlignmentX(CENTER_ALIGNMENT);
         bg.add(tittle);
 
-        resultado = new JLabel("(Seleccione un archivo)");
-        resultado.setBounds(120, 100, 150, 20);
-        resultado.setAlignmentX(LEFT_ALIGNMENT);
-        bg.add(resultado);
+        result = new JLabel("(Seleccione un archivo)");
+        result.setBounds(120, 100, 150, 20);
+        result.setAlignmentX(LEFT_ALIGNMENT);
+        bg.add(result);
     }
 
     private void putButtons() {
@@ -65,7 +66,7 @@ public class MyFrame extends JFrame implements ActionListener {
         bg.add(chooseButton);
     }
     
-    @Override
+   @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == chooseButton) {
             JFileChooser txtchooser = new JFileChooser();
@@ -75,11 +76,13 @@ public class MyFrame extends JFrame implements ActionListener {
                 try {
                     BufferedReader reader = new BufferedReader(new FileReader(file));
                     String input = reader.readLine(); // aqui esta el String que va dentro de calculate
-                    reader.close(); 
+                    // aqui se coloca actualiza el label para dar el resultado que devuelve calculate
+                    result.setText(String.valueOf(pila.calculate(input))); // implementar pila.calculate!!!
+                    reader.close(); // si algo esta fallando pueden probar cambiar de lugar las lineas 80 y 81
                 } catch (IOException err) {
                     err.printStackTrace();
                 }
-		    }
+	     }
 
         }
         
